17db22df6935bf07154a29aef0c39c1f0341c4b1
Kiswendsida Abel Ouedraogo <halloween452001@yahoo.fr>
2022-05-05 18:27:59 +0200

    adding admin board and use model

--- src/app/board-admin/board-admin.component.ts
+++ src/app/board-admin/board-admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
+import { AuthService } from '../_services/auth.service';
+import { User } from '../models/user.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-board-admin',
@@ -8,8 +11,15 @@ import { UserService } from '../_services/user.service';
 })
 export class BoardAdminComponent implements OnInit {
   content?: string;
+  users?: User[];
+  currentUser: User = {};
+  currentIndex = -1;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, 
+    private  authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.userService.getAdminBoard().subscribe({
@@ -19,6 +29,63 @@ export class BoardAdminComponent implements OnInit {
       error: err => {
         this.content = JSON.parse(err.error).message;
       }
+    });
+
+    this.retrieveUsers();
+  }
+
+  retrieveUsers(): void {
+    this.authService.getAll()
+    .subscribe({
+      next: (data) => {
+        this.users = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e)
+    });
+  }
+
+  refreshList(): void {
+    this.retrieveUsers();
+    this.currentUser = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveUser(user: User, index: number): void {
+    this.currentUser = user;
+    this.currentIndex = index;
+  }
+
+  removeAllUsers(): void {
+    this.authService.deleteAll()
+    .subscribe({
+      next: (res) => {
+        console.log(res);
+        this.refreshList();
+      },
+      error: (e) => console.error()
+    });
+  }
+
+  getUser(id: string): void {
+    this.authService.get(id)
+    .subscribe({
+      next: (data) => {
+        this.currentUser = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e)
+    });
+  }
+
+  deleteUser(): void {
+    this.authService.delete(this.currentUser.id)
+    .subscribe({
+      next: (res) => {
+        console.log(res);
+        this.router.navigate(['/admin']);
+      },
+      error: (e) => console.error(e)
     });
   }
 }
